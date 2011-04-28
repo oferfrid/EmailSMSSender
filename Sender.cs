b7ebcd769f404b11a5ac11bbea8d190f63ee924e
Ofer Fridman <oferfrid@gmail.com>
2011-04-28 10:45:21 +0300

    Working Version with Sipko

---
+++ Sender.cs
@@ -0,0 +1,67 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: oferfrid
+ * Date: 13/02/2009
+ * Time: 14:48
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Configuration;
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailSMSSender
+{
+	/// <summary>
+	/// Description of EmailSender.
+	/// </summary>
+	public static class Sender
+	{
+		
+		
+		private static void SendEmail(string FromGmailEmail,
+		                              string GmailPassword ,
+		                              string ToEmail,
+		                              string Subject,
+		                              string Body)
+		{
+			
+			System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage();
+			msg.To.Add(ToEmail);
+			msg.Subject = Subject;
+			msg.Body = Body;
+			msg.IsBodyHtml = false;
+
+			SmtpClient SMPclint = new SmtpClient();
+			SMPclint.Host = "smtp.gmail.com";
+			SMPclint.EnableSsl = true;
+			NetworkCredential NetCry = new NetworkCredential();
+			NetCry.UserName = FromGmailEmail;
+			NetCry.Password = GmailPassword;
+			SMPclint.UseDefaultCredentials = true;
+			SMPclint.Credentials = NetCry;
+			SMPclint.EnableSsl = true;
+			// add the following two sentence.
+			MailAddress fromMailAddr = new MailAddress(FromGmailEmail,"Automatic Email Sender");
+			msg.From = fromMailAddr;
+			SMPclint.Port = 587;
+			SMPclint.Send(msg);
+		}
+		public static void SendSMS(string ToPhone,
+		                           string Body)
+		{
+			string ToEmail =ToPhone + ConfigurationManager.AppSettings["SMSProviderEmailServer"];
+			SendEmail(ToEmail,string.Empty,Body);
+		}
+		public static void SendEmail(string ToEmail,
+		                             string Subject,
+		                             string Body)
+		{
+			string FromGmailEmail = ConfigurationManager.AppSettings["GmailSenderUser"];
+			string FromEmailPassword = ConfigurationManager.AppSettings["GmailSenderPassword"];
+			SendEmail(FromGmailEmail,FromEmailPassword,ToEmail,Subject,Body);
+		}
+	}
+}
---
+++ test/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: oferfrid
+ * Date: 25/06/2009
+ * Time: 09:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace test
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("sending email...");
+			EmailSMSSender.Sender.SendEmail("oferfrid@gmail.com","test","old param test \n" + DateTime.Now.ToString());
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Email send \nPress any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
